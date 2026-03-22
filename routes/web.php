d93bd3fb1c751f6773ffd5350a528241fde762e7
Kerem Ozguven <ozguvenkerem@gmail.com>
2026-03-23 02:00:29 +0300

    fix: add temporary today route to complete auth redirect flow

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/today');
+Route::get('/today', function () {
+    return 'Today';
+});
 
 Route::get('/login', [LoginController::class, 'create'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
